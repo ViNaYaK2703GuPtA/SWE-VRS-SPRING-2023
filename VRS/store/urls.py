7d941f9b55d0d2fdbfff4c582cb939e4d33f8e1d
ViNaYaK2703GuPtA <108051097+ViNaYaK2703GuPtA@users.noreply.github.com>
2023-03-27 21:33:12 +0530

    first submission

--- VRS/store/urls.py
+++ VRS/store/urls.py
@@ -3,9 +3,13 @@ from . import views
 
 urlpatterns = [
 	# Leave as empty string for base url
-	path('', views.store, name="store"),
+	path('', views.role, name="role"),
+	path('store/', views.store, name="store"),
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
+	path('staff/login/', views.staff_login, name='staff_login'),
+        path('customer/login/', views.customer_login, name='customer_login'),
+        path('customer/signup/', views.customer_signup, name='customer_signup'),
 
 	path('update_item/', views.updateItem, name="update_item"),
 	path('process_order/', views.processOrder, name="process_order"),
--- VRS/store/views.py
+++ VRS/store/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 import json
 import datetime
 from .models import *
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 
+def role(request):
+     return render(request, 'store/role.html')
+
 def store(request):
      if request.user.is_authenticated:
+          if( request.user.is_staff == True):
+               return redirect('/admin')
           customer = request.user.customer
           order, created = Order.objects.get_or_create(customer=customer, complete=False)
           items = order.orderitem_set.all()
@@ -99,3 +106,52 @@ def processOrder(request):
           print("User is not logged in!")
 
      return JsonResponse('Payment Complete!', safe=False)
+
+def customer_signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password1 = request.POST.get('password1', '')
+        password2 = request.POST.get('password2', '')
+        email = request.POST.get('email', '')
+
+        if password1 != password2:
+            return render(request, 'store/customer_signup.html', {'message': 'Password is not confirmed!'})
+
+        if User.objects.filter(username=username).exists():
+            return render(request, 'store/customer_signup.html', {'message': 'User already exists!'})
+
+        user = User.objects.create_user(username=username, password=password1, email=email)
+        customer = Customer.objects.create(user=user, name=username, email=email, password=password1)
+
+        login(request, user)
+        return redirect('/customer/login/')
+
+    return render(request, 'store/customer_signup.html', {'message': 'Account Successfully created!'})
+
+        
+
+def customer_login(request):
+     if request.method == 'POST':
+          username = request.POST.get('username', '')
+          password = request.POST.get('password', '')
+          user = authenticate( username=username, password=password)
+          if user is not None:
+               login(request, user)
+               return redirect('/store')
+          else:
+               return render(request, 'store/customer_login.html', {'message': 'Invalid username or password'})
+     
+     return render(request, 'store/customer_login.html', {'message': ''})
+
+def staff_login(request):
+     if request.method == 'POST':
+          username = request.POST.get('username', '')
+          password = request.POST.get('password', '')
+          user = authenticate( username=username, password=password)
+          if user is not None:
+               login(request, user)
+               return redirect('/store')
+          else:
+               return render(request, 'store/staff_login.html', {'message': 'Invalid username or password'})
+     
+     return render(request, 'store/staff_login.html', {'message': ''})
