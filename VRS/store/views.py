51a81f692ad5cf9aa4735b4efcb5ed2bed12f881
ishaaniitkgp <128364279+ishaaniitkgp@users.noreply.github.com>
2023-03-27 15:11:34 +0530

    5th commit

--- VRS/store/views.py
+++ VRS/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.contrib.auth import authenticate, login, logout
   
 # Create your views here.
 
@@ -40,3 +41,51 @@ def checkout(request):
      
      context = {'items': items, 'order': order}
      return render(request, 'store/checkout.html', context)
+
+
+def customer_signup(request):
+     if request.method == 'POST':
+          username = request.POST.get('username', '')
+          password1 = request.POST.get('password1', '')
+          password2 = request.POST.get('password2', '')
+          email = request.POST.get('email', '')
+
+          if password1 != password2 : 
+              return render(request, 'store/customer_signup.html', {'message': 'Password is not confirmed!'})
+          if len(User.objects.filter(username=username))!=0:
+               return render(request, 'store/customer_signup.html', {'message': 'User already exists!'})
+          
+          user = User.objects.create_user(username=username, password=password1, email=email)
+          
+          user.save()
+          return redirect('/customer/login/')
+     
+     return render(request, 'store/customer_signup.html', {'message': 'Account Succesfully created!'})
+
+        
+
+def customer_login(request):
+     if request.method == 'POST':
+          username = request.POST.get('username', '')
+          password = request.POST.get('password', '')
+          user = authenticate( username=username, password=password)
+          if user is not None:
+               login(request, user)
+               return redirect('/store')
+          else:
+               return render(request, 'store/customer_login.html', {'message': 'Invalid username or password'})
+     
+     return render(request, 'store/customer_login.html', {'message': ''})
+
+def staff_login(request):
+     if request.method == 'POST':
+          username = request.POST.get('username', '')
+          password = request.POST.get('password', '')
+          user = authenticate( username=username, password=password)
+          if user is not None:
+               login(request, user)
+               return redirect('/store')
+          else:
+               return render(request, 'store/staff_login.html', {'error_message': 'Invalid username or password'})
+     
+     return render(request, 'store/staff_login.html', {'error_message': ''})
