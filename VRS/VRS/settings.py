cc98880f00d9745e5fa0d02a565d6f8102dc4540
ishaaniitkgp <128364279+ishaaniitkgp@users.noreply.github.com>
2023-04-01 18:39:17 +0530

    Update settings.py
    added line(refer line no.133)

--- VRS/VRS/settings.py
+++ VRS/VRS/settings.py
@@ -130,6 +130,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
 
+LOGOUT_REDIRECT_URL = 'role'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
